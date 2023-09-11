b6b86d3e4ef53d9b402fcd02243a410a7f1fd1a7
William Leaver <william.leaver@snhu.edu>
2023-09-11 14:10:08 -0500

    updated class to className and preparing to deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import logo from '../assets/images/logo.png'; // with import
 export default function Home() {
   return (
       <main> 
-        <div class="flex justify-center items-center">
-              <Image class="object-center scale-90 hover:scale-100 transform-gpu" src={logo} alt="LOGO" />
+        <div className="flex justify-center items-center">
+              <Image className="object-center scale-90 hover:scale-100 transform-gpu" src={logo} alt="LOGO" />
           </div>
-          <div>
-              <h1 class="text-center text-3xl font-bold hover:underline"> UNDER CONSTRUCTION</h1>
+          <div className="text-center text-3xl font-bold hover:underline">
+              <h1> UNDER CONSTRUCTION</h1>
+              <a href="https://www.instagram.com/the.craftywildflower/?hl=en"> Instagram: @the.craftywildflower</a>
           </div>
       </main>
   )
