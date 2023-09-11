6fed6b0764bf55cc0daefa62122055820d1d9079
William Leaver <william.leaver@snhu.edu>
2023-09-11 12:09:19 -0500

    added About+Shop pages and moved NavBar to layout

---
+++ src/app/About/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>about</div>
+  )
+}
---
+++ src/app/Shop/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ShopLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+    <div>Shop</div>
+  )
+}
